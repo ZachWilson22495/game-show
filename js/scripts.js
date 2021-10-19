8e153a1174a6e39385518d4fa17cc25928ffc2ce
Zach Wilson <zwilson22495@gmail.com>
2021-10-19 13:33:24 -0700

    Created game-show.html, styles.css, bootstrap.css, scripts.js, jquery.js, and an img folder. These files interact to build an interactive game show where you choose one of three hidden prizes.
    Co-authored-by Anna Pittman <annaepittman@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".apples-choice").click(function() {
+    $("#apples-showing").toggle();
+    $("#apples-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".garbage-choice").click(function() {
+    $("#garbage-showing").toggle();
+    $("#garbage-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".respect-choice").click(function() {
+    $("#respect-showing").toggle();
+    $("#respect-hidden").toggle();
+  });
+});
